d7ec25758d90c9c7634dabe2c6667aa9c68ac726
stefan9541 <45336682+stefan9541@users.noreply.github.com>
2023-05-16 23:47:32 +0300

    minor rafactoring

--- src/Components/about-our/about-our.js
+++ src/Components/about-our/about-our.js
@@ -1,37 +1,33 @@
-import './about-our.css'
-import girl from '../img/girl.jpg';
-import Coffee from '../header/svg/Coffee.svg';
-
+import "./about-our.css";
+import girl from "../img/girl.jpg";
+import Coffee from "../header/svg/Coffee.svg";
 
 const AboutOur = () => {
-    return(
-        <div className='About-our-beans'>
-        <div className='about-our'>
-            <div className='img-block'>
-            <img src={girl} alt="girl" />
-            </div>
-            <div className='about-block'>
-            <h2 className='title-about'>About our beans</h2>
-            <div className='coffee-block'>
-            <div className='line'></div>
-            <img src={Coffee} alt="swg" className='coffee-swg' />
-            <div className='line'></div>
-            </div>
-            <p className='text-abaut'>
-            Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.
-
-Afraid at highly months do things on at. Situation recommend objection do intention
-so questions. 
-As greatly removed calling pleased improve an. Last ask him cold feel
-met spot shy want. Children me laughing we prospect answered followed. At it went
-is song that held help face.
-            </p>
-            
-            </div>
-            
+  return (
+    <div className="About-our-beans">
+      <div className="about-our">
+        <div className="img-block">
+          <img src={girl} alt="girl" />
         </div>
-        <div className='line-b'></div>
+        <div className="about-block">
+          <h2 className="title-about">About our beans</h2>
+          <div className="coffee-block">
+            <div className="line"></div>
+            <img src={Coffee} alt="swg" className="coffee-swg" />
+            <div className="line"></div>
+          </div>
+          <p className="text-abaut">
+            Extremity sweetness difficult behaviour he of. On disposal of as
+            landlord horrible. Afraid at highly months do things on at.
+            Situation recommend objection do intention so questions. As greatly
+            removed calling pleased improve an. Last ask him cold feel met spot
+            shy want. Children me laughing we prospect answered followed. At it
+            went is song that held help face.
+          </p>
         </div>
-    )
-}
+      </div>
+      <div className="line-b"></div>
+    </div>
+  );
+};
 export default AboutOur;
--- src/Components/app/app.js
+++ src/Components/app/app.js
@@ -1,89 +1,77 @@
 /* import { Component } from "react"; */
-import Header from '../header/header';
-import OurCoffee from '../our-coffee/our-coffee';
-import AboutOur from '../about-our/about-our';
-import LookiingFor from '../lookiing-for/lookiing-for';
-import AppFilter from '../filter-item/filter-item'
-import CoffeeItem from '../coffee-item/coffee-item';
-import Footer from '../footer/footer'
-import { Component } from 'react';
+import Header from "../header/header";
+import OurCoffee from "../our-coffee/our-coffee";
+import AboutOur from "../about-our/about-our";
+import LookiingFor from "../lookiing-for/lookiing-for";
+import AppFilter from "../filter-item/filter-item";
+import CoffeeItem from "../coffee-item/coffee-item";
+import Footer from "../footer/footer";
+import { Component } from "react";
 
-import './app.css';
-
+import "./app.css";
 
 class App extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            data: [
-                {name: 'Arabica Coffee 1 kg', type: 'Brazil', price: 6.99, id: 1},
-                {name: 'Robusta Coffee 1 kg', type: 'Kenya', price: 6.99, id: 2},
-                {name: 'Liberica Coffee 1 kg', type: 'Columbia', price: 6.99, id: 3},
-                {name: 'Excelsa Coffee 1 kg', type: 'Brazil', price: 6.99, id: 4},
-                {name: 'Mocha Coffee 1 kg', type: 'Brazil', price: 6.99, id: 5},
-                {name: 'Maragogipe Coffee 1 kg', type: 'Brazil', price: 6.99, id: 6}
-            ],
-            term: '',
-            filter: 'all'
-        }
-        this.maxId = 7;
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: "Arabica Coffee 1 kg", type: "Brazil", price: 6.99, id: 1 },
+        { name: "Robusta Coffee 1 kg", type: "Kenya", price: 6.99, id: 2 },
+        { name: "Liberica Coffee 1 kg", type: "Columbia", price: 6.99, id: 3 },
+        { name: "Excelsa Coffee 1 kg", type: "Brazil", price: 6.99, id: 4 },
+        { name: "Mocha Coffee 1 kg", type: "Brazil", price: 6.99, id: 5 },
+        { name: "Maragogipe Coffee 1 kg", type: "Brazil", price: 6.99, id: 6 },
+      ],
+      term: "",
+      filter: "all",
+    };
+    this.maxId = 7;
+  }
+
+  searchEmp = (items, term) => {
+    if (term.length === 0) {
+      return items;
     }
+    return items.filter((item) => {
+      return item.name.includes(term);
+    });
+  };
 
-searchEmp = (items, term) => {
-   if (term.legth === 0) {
-    return items;
-   }
-return items.filter(item => {
-    return item.name.indexOf(term) > -1
-})
-}
+  onUpdateSearch = (term) => {
+    this.setState({ term });
+  };
 
-onUpdateSearch = (term) => {
-    this.setState({term});
-}
+  filterPost = (items, filter) => {
+    return filter === "all"
+      ? items
+      : items.filter(({ type }) => type === filter);
+  };
 
-filterPost = (items, filter) => {
-    switch (filter) {
-        case 'Brazil':
-            return items.filter(item => item.type == 'Brazil');
-        case 'Kenya':
-            return items.filter(item => item.type == 'Kenya');
-        case 'Columbia':
-            return items.filter(item => item.type == 'Columbia');
-        default:
-            return items
-    }
+  onFilterSelect = (filter) => {
+    this.setState({ filter });
+  };
 
-}
-
-onFilterSelect = (filter) => {
-    this.setState({filter});
-}
-
-render() {
-    const {data, term, filter} = this.state;
+  render() {
+    const { data, term, filter } = this.state;
     const visidleData = this.filterPost(this.searchEmp(data, term), filter);
 
-    return(
-        <div className="app">
-            <Header />
-            <OurCoffee/>
-            <AboutOur/>
-            <div className='lookiing-for'>
-            <LookiingFor 
+    return (
+      <div className="app">
+        <Header />
+        <OurCoffee />
+        <AboutOur />
+        <div className="lookiing-for">
+          <LookiingFor
             onUpdateSearch={this.onUpdateSearch}
-            onFilterSelect={this.onFilterSelect}/> 
-            <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
-            </div>
-            <CoffeeItem
-            data={visidleData}/>
-            <Footer/>
+            onFilterSelect={this.onFilterSelect}
+          />
+          <AppFilter filter={filter} onFilterSelect={this.onFilterSelect} />
         </div>
-
+        <CoffeeItem data={visidleData} />
+        <Footer />
+      </div>
     );
+  }
 }
-
-}
-
 
 export default App;
